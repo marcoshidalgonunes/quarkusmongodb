ff3c885d1ac70124549462f94e7f5453b74cf3cc
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-08-22 21:34:07 -0300

    Implemented CRUD endpoints

--- src/main/java/org/quarkusmongodb/routes/BookRoute.java
+++ src/main/java/org/quarkusmongodb/routes/BookRoute.java
@@ -2,11 +2,15 @@ package org.quarkusmongodb.routes;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.quarkusmongodb.models.Book;
 import org.quarkusmongodb.repositories.BookRepository;
 
+import io.quarkus.vertx.web.Body;
+import io.quarkus.vertx.web.Param;
 import io.quarkus.vertx.web.Route;
 import io.quarkus.vertx.web.RouteBase;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 
 @RouteBase(path = "api")
@@ -17,5 +21,33 @@ public class BookRoute {
     @Route(path = "/Books", methods = Route.HttpMethod.GET) 
     List<Book> getAll() {
         return bookRepository.listAll();
+    }
+
+    @Route(path = "/Books/:id", methods = Route.HttpMethod.GET) 
+    List<Book> getByCriteria(@Param String criteria, @Param String search) {
+        return bookRepository.find(criteria, search).list();
+    }
+
+    @Route(path = "/Books/:id", methods = Route.HttpMethod.GET) 
+    Book getById(@Param String id) {
+        return bookRepository.findById(new ObjectId(id));
+    }
+
+    @Route(path = "/Books", methods = Route.HttpMethod.POST) 
+    Uni<Book> create(@Body Book book) {
+         bookRepository.persist(book);
+         return Uni.createFrom().item(book);
+    }   
+
+    @Route(path = "/Books", methods = Route.HttpMethod.PUT) 
+    Uni<Void> update(@Body Book book) {
+         bookRepository.update(book);
+         return Uni.createFrom().nullItem();
+    }   
+
+    @Route(path = "/Books/:id", methods = Route.HttpMethod.DELETE) 
+    Uni<Void> delete(@Param String id) {
+        bookRepository.deleteById(new ObjectId(id));
+         return Uni.createFrom().nullItem();
     }
 }
